011a6de56b1fa10bbc8c4250d8cd97ba04669d69
louis59240 <bourgeois.louis.59240@gmail.COm>
2024-08-05 05:29:52 +0200

    handling of the vpc ip adress in the config.js file

--- client/taskly/config.js
+++ client/taskly/config.js
@@ -4,13 +4,16 @@ const getApiUrl = () => {
     const hostname = window.location.hostname;
     if (hostname === "localhost" || hostname === "127.0.0.1") {
       return "http://localhost:3001";
+    } else if (hostname === "192.168.1.100" || hostname === "89.116.111.43") {
+      // Si nous sommes sur l'IP locale ou l'IP du VPS
+      return `http://${hostname}:3001`;
     } else {
-      // Si nous ne sommes pas sur localhost, utilisez l'IP du réseau
-      return "http://192.168.1.100:3001";
+      // Pour tout autre cas, utilisez l'IP du VPS
+      return "http://89.116.111.43:3001";
     }
   } else {
-    // Nous sommes côté serveur, utilisez une valeur par défaut ou une variable d'environnement
-    return "http://192.168.1.100:3001";
+    // Nous sommes côté serveur, utilisez l'IP du VPS
+    return "http://89.116.111.43:3001";
   }
 };
 
