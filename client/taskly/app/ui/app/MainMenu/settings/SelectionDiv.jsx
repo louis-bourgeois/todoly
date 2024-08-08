1b851ef78945de3b2d1e17ccf23c05a490c5ffcd
louis59240 <bourgeois.louis.59240@gmail.COm>
2024-08-08 11:53:21 +0200

    improve the view menu dropdown menu click

--- client/taskly/app/ui/app/MainMenu/settings/SelectionDiv.jsx
+++ client/taskly/app/ui/app/MainMenu/settings/SelectionDiv.jsx
@@ -1,6 +1,7 @@
-export default function SelectionDiv({ children, className }) {
+export default function SelectionDiv({ children, className, onClick }) {
   return (
     <div
+      onClick={onClick}
       className={`border border-secondary h-[4.5vh] flex justify-between rounded-[10px] items-center ${className}`}
     >
       {children}
