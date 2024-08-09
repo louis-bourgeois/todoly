0f40501f89bafa0fa3f056e8f30168939d769a59
louis59240 <bourgeois.louis.59240@gmail.COm>
2024-08-09 15:10:38 +0200

    fix the width of the cards in all page and remove the unncessary height prop in the settings of the slick carousel in currently

--- client/taskly/app/ui/app/Slide.jsx
+++ client/taskly/app/ui/app/Slide.jsx
@@ -3,7 +3,7 @@ export default function Slide({ children, index }) {
     <div className="h-[97.5vh]">
       <div
         key={index}
-        className="5xl:h-[83.5%] 4.5xl:h-[84.5%] 4xl:h-[83.5%] 3xl:h-[86.5%] 2xl:h-[87.5%] xl:h-[88.5%] lg:h-[90%]"
+        className=" 5xl:h-[83.5%] 4.5xl:h-[84.5%] 4xl:h-[83.5%] 3xl:h-[86.5%] 2xl:h-[87.5%] xl:h-[88.5%] lg:h-[90%]"
       >
         <div
           key={index}
