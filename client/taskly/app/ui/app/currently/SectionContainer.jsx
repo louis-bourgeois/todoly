c35dbc40451cd9d4bb0abcaee68a210cb08c8674
louis59240 <bourgeois.louis.59240@gmail.COm>
2024-08-11 11:31:39 +0200

    fix a flex spacing issue in the editing section on desktop

--- client/taskly/app/ui/app/currently/SectionContainer.jsx
+++ client/taskly/app/ui/app/currently/SectionContainer.jsx
@@ -174,9 +174,9 @@ const SectionContainer = ({
                     </h1>
                   )}
                   {section.name !== "Other" && (
-                    <>
+                    <div className="flex justify-between items-center">
                       <button
-                        className="ml-5 text-secondary hover:text-dominant"
+                        className=" text-secondary hover:text-dominant"
                         onClick={() =>
                           handleEditSection(section.id, section.name)
                         }
@@ -211,7 +211,7 @@ const SectionContainer = ({
                         </svg>
                       </button>
                       <button
-                        className="ml-3 text-important"
+                        className="text-important"
                         onClick={() => handleDeleteSection(section.id)}
                       >
                         <svg
@@ -229,39 +229,38 @@ const SectionContainer = ({
                             strokeLinejoin="round"
                           ></g>
                           <g id="SVGRepo_iconCarrier">
-                            {" "}
                             <path
                               d="M3 6.98996C8.81444 4.87965 15.1856 4.87965 21 6.98996"
                               stroke="#FA3766"
                               strokeWidth="1.5"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                            ></path>{" "}
+                            ></path>
                             <path
                               d="M8.00977 5.71997C8.00977 4.6591 8.43119 3.64175 9.18134 2.8916C9.93148 2.14146 10.9489 1.71997 12.0098 1.71997C13.0706 1.71997 14.0881 2.14146 14.8382 2.8916C15.5883 3.64175 16.0098 4.6591 16.0098 5.71997"
                               stroke="#FA3766"
                               strokeWidth="1.5"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                            ></path>{" "}
+                            ></path>
                             <path
                               d="M12 13V18"
                               stroke="#FA3766"
                               strokeWidth="1.5"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                            ></path>{" "}
+                            ></path>
                             <path
                               d="M19 9.98999L18.33 17.99C18.2225 19.071 17.7225 20.0751 16.9246 20.8123C16.1266 21.5494 15.0861 21.9684 14 21.99H10C8.91389 21.9684 7.87336 21.5494 7.07541 20.8123C6.27745 20.0751 5.77745 19.071 5.67001 17.99L5 9.98999"
                               stroke="#FA3766"
                               strokeWidth="1.5"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                            ></path>{" "}
+                            ></path>
                           </g>
                         </svg>
                       </button>
-                    </>
+                    </div>
                   )}
                 </div>
               </div>
